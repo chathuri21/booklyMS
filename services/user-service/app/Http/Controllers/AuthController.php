2ec4736563ed742e4468505caf04636b8bb23a30
chathuri21 <chathuri21@gmail.com>
2025-11-24 20:12:26 +0100

    Created event for user creation

--- services/user-service/app/Http/Controllers/AuthController.php
+++ services/user-service/app/Http/Controllers/AuthController.php
@@ -3,8 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use App\Events\UserCreated;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'role' => $request->role,
+        ]);
+
+        event(new UserCreated(
+            $user->id,
+            $user->name,
+            $user->email,
+            $user->role
+        ));
+        
+        return response()->json($user, 201);
+    }
 }
