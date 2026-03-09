13174ee2fa1adca781534e331c4fc0549376006b
chathuri21 <chathuri21@gmail.com>
2026-03-09 14:45:39 +0100

    Add Publisher test

---
+++ services/user-service/tests/Unit/Listeners/PublishUserCreatedTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Unit\Listeners;
+
+use App\Domain\Services\LoggerInterface;
+use App\Events\UserCreated;
+use App\Listeners\PublishUserCreated;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class PublishUserCreatedTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_listener_handles_event(): void
+    {
+        $loggerMock = $this->createMock(LoggerInterface::class);
+
+        $user = User::factory()->create();
+
+        $event = new UserCreated($user, $loggerMock);
+
+        $listener = new PublishUserCreated($loggerMock);
+        $listener->handle($event);
+        
+        $this->assertTrue(true);
+    }
+}
