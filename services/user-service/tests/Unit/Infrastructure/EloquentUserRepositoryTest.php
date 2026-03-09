f946171f664078eae2698fe90b23727bdab7010c
chathuri21 <chathuri21@gmail.com>
2026-03-09 14:27:16 +0100

    Add Repository test

---
+++ services/user-service/tests/Unit/Infrastructure/EloquentUserRepositoryTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Unit\Infrastructure\EloquentUserRepositoryTest;
+use App\Domain\DTOs\RegisterUserDTO;
+use App\Infrastructure\Persistence\EloquentUserRepository;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+
+class EloquentUserRepositoryTest extends TestCase
+{
+    use RefreshDatabase;
+    
+    public function test_create_user(): void
+    {
+        $repository = new EloquentUserRepository();
+
+        $dto = new RegisterUserDTO(
+            name: 'Test User',
+            email: 'test@example.com',
+            phone: '1234567890',
+            password: 'password',
+            role: 'customer'
+        );
+
+        $user = $repository->create($dto);
+
+        $this->assertNotNull($user);
+        $this->assertEquals($dto->name, $user->name);
+        $this->assertEquals($dto->email, $user->email); 
+        $this->assertEquals($dto->phone, $user->phone);
+        $this->assertEquals($dto->role, $user->role);
+    }
+}
