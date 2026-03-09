9abfbefb22005822c9888cd3208f41370aa43f2a
chathuri21 <chathuri21@gmail.com>
2026-03-09 21:23:36 +0100

    Updated global exception handler with new exception classes

--- services/user-service/app/Exceptions/Handler.php
+++ services/user-service/app/Exceptions/Handler.php
@@ -2,20 +2,23 @@
 
 namespace App\Exceptions;
 
+use App\Domain\Exceptions\InactiveAccountException;
+use App\Domain\Exceptions\InvalidCredentialsException;
 use App\Domain\Exceptions\UserAlreadyExistsException;
 use Exception;
+use Illuminate\Validation\Rules\In;
 use Throwable;
 
 class Handler extends Exception
 {
     public function render($request, Throwable $e)
     {
-        if ($e instanceof UserAlreadyExistsException) {
+        if ($e instanceof UserAlreadyExistsException || $e instanceof InvalidCredentialsException || $e instanceof InactiveAccountException) {
             return response()->json([
                 'message' => $e->getMessage(),
-            ], $e->getCode() ?: 409);
+            ], 409);
         }
-        
+
         return response()->json([
             'message' => $this->getMessage(),
         ], 500);
