12024eb00ed03eb043759a1d1430af87ca9678f7
chathuri21 <chathuri21@gmail.com>
2026-03-10 13:40:39 +0100

    Created job to dispatche the RabbitMQ messages

--- services/user-service/app/Listeners/PublishUserCreated.php
+++ services/user-service/app/Listeners/PublishUserCreated.php
@@ -2,8 +2,9 @@
 
 namespace App\Listeners;
 
-use App\Events\UserCreated;
 use App\Domain\Services\LoggerInterface;
+use App\Events\UserCreated;
+use App\Jobs\PublishUserCreatedJob;
 use Illuminate\Support\Facades\Queue;
 
 class PublishUserCreated
@@ -16,27 +17,12 @@ class PublishUserCreated
     // so that other services can consume it and react accordingly (e.g., send welcome email, update search index, etc.)
     public function handle(UserCreated $event): void
     {
-        $this->logger->info('UserCreated publish triggered');
+        $this->logger->info('PublishUserCreated triggered');
         // Here you would implement the logic to publish the event to your message broker
         // For example, using a RabbitMQ client or Laravel's built-in queue system
 
         $user = $event->user;
-
-        $this->logger->info('UserCreated published payload', ['payload' => $user]);
-
-        $payload = json_encode([
-            'event' => 'user.created',
-            'data' => [
-                'user_id' => $user->id,
-                'name' => $user->name,
-                'email' => $user->email,
-                'phone' => $user->phone,
-                'role' => $user->role,
-                'is_active' => $user->is_active,
-            ]
-        ]);
         
-        $this->logger->info('UserCreated published to message broker', ['payload' => $payload]);
-        Queue::connection('rabbitmq')->pushRaw($payload, 'user.created');
+        PublishUserCreatedJob::dispatch(json_encode(['event' => 'user.created','data' => $user->toArray()]));
     }
 }
