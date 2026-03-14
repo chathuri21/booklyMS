3c9a8d3d8bbb3edc0a2424356d377029fb44f710
chathuri21 <chathuri21@gmail.com>
2026-03-14 14:55:28 +0100

    Update User entity for Login and register both

--- services/user-service/app/Domain/Entities/User.php
+++ services/user-service/app/Domain/Entities/User.php
@@ -2,11 +2,23 @@
 
 namespace App\Domain\Entities;
 
+use App\Models\User as EloquentUser;
+
 class User
 {
     public function __construct(
+        public ?int $id,
+        public string $name,
         public string $email,
+        public string $phone,
         public string $password,
-        public bool $is_active
+        public string $role,
+        public bool $isActive,
+        private ?EloquentUser $eloquentUser = null
     ) {}
+
+    public function getEloquentModel(): EloquentUser
+    {
+        return $this->eloquentUser;
+    }
 }
