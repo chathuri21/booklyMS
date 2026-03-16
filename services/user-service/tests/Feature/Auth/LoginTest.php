6c64f18656094595aa619e658c9677fe42decc41
chathuri21 <chathuri21@gmail.com>
2026-03-16 21:26:46 +0100

    Login feature test

---
+++ services/user-service/tests/Feature/Auth/LoginTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Hash;
+use Tests\TestCase;
+
+class LoginTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_user_can_login() : void 
+    {
+        $user = User::factory()->create([
+            'email' => 'test@example.com',
+            'password' => Hash::make('password'),
+            'is_active' => true
+        ]);
+
+        $response = $this->postJson('/api/login', [
+            'email' => 'test@example.com',
+            'password' => 'password'
+        ]);
+
+        $response->assertStatus(200)
+            ->assertJson([
+                'message' => 'Login successful',
+            ])
+            ->assertJsonStructure([
+                'message',
+                'data' => [
+                    'user' => [
+                        'id',
+                        'name',
+                        'email',
+                        'role',
+                    ],
+                    'access_token',
+                    'token_type',
+                ]
+            ]);
+    }
+}
