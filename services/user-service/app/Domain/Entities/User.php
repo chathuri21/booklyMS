da715bbb52de984c203366b3b7e737a2f3b5a4f6
chathuri21 <chathuri21@gmail.com>
2026-03-18 14:51:22 +0100

    Updated with logic

--- services/user-service/app/Domain/Entities/User.php
+++ services/user-service/app/Domain/Entities/User.php
@@ -2,6 +2,9 @@
 
 namespace App\Domain\Entities;
 
+use App\Domain\Exceptions\InactiveAccountException;
+use App\Domain\Exceptions\InvalidCredentialsException;
+
 use App\Models\User as EloquentUser;
 
 class User
@@ -20,5 +23,19 @@ class User
     public function getEloquentModel(): EloquentUser
     {
         return $this->eloquentUser;
+    }
+
+    public function checkPassword(string $password) : void 
+    {
+        if (!password_verify($password, $this->password)) {
+            throw new InvalidCredentialsException();
+        }
+    }
+
+    public function ensureIsActive(): void 
+    {
+        if (!$this->isActive) {
+            throw new InactiveAccountException();
+        }
     }
 }
