9e7c2c938ba561167529ff1c7927338c6dc5bf34
chathuri21 <chathuri21@gmail.com>
2026-03-18 13:16:32 +0100

    More feature test for login

--- services/user-service/tests/Feature/Auth/LoginTest.php
+++ services/user-service/tests/Feature/Auth/LoginTest.php
@@ -2,7 +2,9 @@
 
 namespace Tests\Feature\Auth;
 
+use App\Exceptions\Handler;
 use App\Models\User;
+use Illuminate\Contracts\Debug\ExceptionHandler;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
@@ -11,13 +13,18 @@ class LoginTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_user_can_login() : void 
+    private function makeFactoryUser(bool $isActive = true): void
     {
-        $user = User::factory()->create([
+        User::factory()->create([
             'email' => 'test@example.com',
             'password' => Hash::make('password'),
-            'is_active' => true
+            'is_active' => $isActive
         ]);
+    }
+
+    public function test_user_can_login() : void 
+    {
+        $this->makeFactoryUser();
 
         $response = $this->postJson('/api/login', [
             'email' => 'test@example.com',
@@ -40,6 +47,82 @@ class LoginTest extends TestCase
                     'access_token',
                     'token_type',
                 ]
+            ]);
+    }
+
+    public function test_login_email_is_require() : void 
+    {
+        $response = $this->postJson('/api/login', [
+            'email' => '',
+            'password' => 'password'
+        ]);
+
+        $response->assertStatus(422)
+            ->assertJsonValidationErrors(['email']);
+    }
+
+    public function test_login_password_is_require() : void 
+    {
+        $response = $this->postJson('/api/login', [
+            'email' => 'test@example.com',
+            'password' => ''
+        ]);
+
+        $response->assertStatus(422)
+            ->assertJsonValidationErrors(['password']);
+    }
+
+    public function test_login_email_must_be_valid() : void 
+    {
+        $response = $this->postJson('/api/login', [
+            'email' => 'invalid-email',
+            'password' => 'password'
+        ]);
+
+        $response->assertStatus(422)
+            ->assertJsonValidationErrors(['email']);
+    }
+
+    public function test_login_fails_with_invalid_password() : void 
+    {
+        $this->makeFactoryUser();
+
+        $response = $this->postJson('/api/login', [
+            'email' => 'test@example.com',
+            'password' => 'wrong-password'
+        ]);
+
+        $response->assertStatus(401)
+            ->assertJson([
+                'message' => 'Invalid credentials provided'
+            ]);
+    }
+
+    public function test_login_user_not_found() : void 
+    {
+        $response = $this->postJson('/api/login', [
+            'email' => 'missing@example.com',
+            'password' => 'password'
+        ]);
+
+        $response->assertStatus(401)
+            ->assertJson([
+                'message' => 'Invalid credentials provided'
+            ]);
+    }
+
+    public function test_login_fails_with_inactive_account() : void 
+    {
+        $this->makeFactoryUser(false);
+
+        $response = $this->postJson('/api/login', [
+            'email' => 'test@example.com',
+            'password' => 'password'
+        ]);
+
+        $response->assertStatus(403)
+            ->assertJson([
+                'message' => 'User account is inactive'
             ]);
     }
 }
