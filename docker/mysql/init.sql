b4e439f02b88d17301d860e06cc509a9ea0fbca2
chathuri21 <chathuri21@gmail.com>
2026-04-24 21:35:57 +0200

    Add mysql init file to create user and apooinment services

--- docker/mysql/init.sql
+++ docker/mysql/init.sql
@@ -1,2 +1,5 @@
+CREATE DATABASE IF NOT EXISTS user_service_db;
 CREATE DATABASE IF NOT EXISTS appointment_service_db;
+GRANT ALL PRIVILEGES ON user_service_db.* TO 'microservice_user'@'%';
 GRANT ALL PRIVILEGES ON appointment_service_db.* TO 'microservice_user'@'%';
+FLUSH PRIVILEGES;
