bd0ab9589f98903d0469e783b419b8c4298b7163
chathuri21 <chathuri21@gmail.com>
2026-07-05 20:38:17 +0200

    Add notification service db

--- docker/mysql/init.sql
+++ docker/mysql/init.sql
@@ -1,5 +1,7 @@
 CREATE DATABASE IF NOT EXISTS user_service_db;
 CREATE DATABASE IF NOT EXISTS appointment_service_db;
+CREATE DATABASE IF NOT EXISTS notification_service_db;
 GRANT ALL PRIVILEGES ON user_service_db.* TO 'microservice_user'@'%';
 GRANT ALL PRIVILEGES ON appointment_service_db.* TO 'microservice_user'@'%';
+GRANT ALL PRIVILEGES ON notification_service_db.* TO 'microservice_user'@'%';
 FLUSH PRIVILEGES;
