8dd1c4489500efcae4cbe432e64777959a113cbd
chathuri21 <chathuri21@gmail.com>
2026-07-05 20:36:51 +0200

    user sanpshot table to notification service

---
+++ services/notification-service/database/migrations/2026_07_05_000000_create_user_snapshots_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_snapshots', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->unique(); // original ID from User Service
+            $table->string('name');
+            $table->string('email')->nullable();
+            $table->string('phone')->nullable();
+            $table->enum('role', ['customer', 'provider', 'admin'])->default('customer');
+            $table->boolean('is_active')->default(true);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_snapshots');
+    }
+};
